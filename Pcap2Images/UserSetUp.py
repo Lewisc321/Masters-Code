a0e178e0be29a2b2c6b40c621d51a909b5d7066f
SHolmes99 <37140707+SHolmes99@users.noreply.github.com>
2023-01-23 00:14:46 +0000

    New Pcap2Image
    Reupload of files, greater input validation

---
+++ Pcap2Images/UserSetUp.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 18:37:19 2023
+
+@author: Callum Manning
+
+Strath Uni - 201720173
+"""
+
+#Example PCAP folder
+EXAMPLE_PCAP_FOLDER = './Packets/'
+
+#SQLI FOLDER
+SQLI_FOLDER = 'SQLIATTACKS/'
+
+#COMSI FOLDER
+COMSI_FOLDER = 'COMSIATTACKS/'
+
+#Brute Force Folder
+BRUTE_FORCE_FOLDER = 'BRUTEFORCEATTACKS/'
+
+#XSS stored Folder
+XSS_STORED_FOLDER = 'XSSSTOREDTTACKS/'
+
+#XSS reflected Folder
+XSS_REFLECTED_FOLDER = 'XSSREFLECTEDATTACKS/'
+
+#XSS DOM Folder
+XSS_DOM_FOLDER = 'XSSDOMATTACKS/'
+
+#Scanned
+SCAN_FOLDER = 'SCANS/'
+
+#Image Out
+IMAGE_OUTPUT = './IMAGE_OUTPUT_1000/'
+
+#PCAP 
+
+#pcap extension
+pcap_extension = '.pcap'
+
+#file slashes
+filePath_slash = '\\'
+
+#Max Pixels 
+pixelDimen = 1000
+max_Pixels = pixelDimen**2 
+
+
+#Image suffix
+colour_suffix = '_colour.png'
+gray_suffix = '_gray.png'
